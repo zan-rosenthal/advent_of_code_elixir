1d61354bbbf9e9b8654b78c4bc645e84d5b533e5
zan-rosenthal <zan.rosenthal@gmail.com>
2019-12-13 15:26:42 -0600

    Solve day 1 puzzle 1

--- lib/day_01/solution_01.ex
+++ lib/day_01/solution_01.ex
@@ -1,18 +1,36 @@
-defmodule Day01.Puzzle01 do
-  @moduledoc """
-  Documentation for AdventOfCode.
-  """
+defmodule Day01.Solution01 do
+  require OpenInput
+  require Enum
 
-  @doc """
-  Hello world.
+  @doc ~S"""
+    Calculates the sum of fuel needed for all modules
 
   ## Examples
 
-      iex> Day01.Puzzle01.calculate_fuel()
-      :world
+      iex> Day01.Solution01.calculate_fuel
+      3490663
 
   """
+
+  @spec calculate_fuel :: number
   def calculate_fuel do
-    :world
+    OpenInput.open('puzzles/day_01/puzzle_01/input.txt')
+      |>Enum.map(&(Integer.parse(&1)))
+      |>Enum.map(fn({ int, _binary }) -> calculate_fuel(int)end)
+      |> Enum.sum
+  end
+
+  @doc ~S"""
+    Calculates fuel needed for a single module given its mass
+
+  ## Examples
+
+      iex> Day01.Solution01.calculate_fuel(19)
+      3
+
+  """
+  @spec calculate_fuel(integer) :: integer
+  def calculate_fuel (fuel) do
+    Integer.floor_div(fuel, 3) - 3
   end
 end
---
+++ test/day_01/solution_01_test.exs
@@ -0,0 +1,4 @@
+defmodule Day01.Solution01Test do
+  use ExUnit.Case, async: true
+  doctest Day01.Solution01
+end
